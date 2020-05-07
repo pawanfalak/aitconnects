f18519f531ae56f9b4d0eeef29cab5a736af11f6
pawanfalak <pawanphalak777@gmail.com>
2020-05-07 06:16:27 +0530

    20 Updated routing and models

---
+++ aitconnects/todo.md
@@ -0,0 +1,25 @@
+1. tweets
+    -creating
+        -text
+        -image
+    -delete
+    -retweet
+2. Users
+    -register
+    -login
+    -logout
+    -profile
+        -image
+        -text
+        -follow button
+    -feed
+        -user's feed only
+        -user's + who they follow?
+
+3. following/followers
+
+
+long term todos
+    -notifications
+    -dm
+    -explore finding hashtags
---
+++ tweets/views.py
@@ -0,0 +1,23 @@
+from django.http import HttpResponse,Http404,JsonResponse
+from django.shortcuts import render
+
+from .models import Tweets
+
+# Create your views here.
+def home_view(request, *args, **kwargs):
+    return HttpResponse("<h1>Hello World!!</h1>")
+
+def tweet_detail_view(request, tweet_id, *args, **kwargs):
+    data={
+        "id": tweet_id,
+    }
+    status = 200
+    try:
+        obj= Tweets.objects.get(id=tweet_id) 
+        data['content'] = obj.content
+    except:
+        data['content'] = "not found"
+        status=404
+
+    
+    return JsonResponse(data,status=status)
