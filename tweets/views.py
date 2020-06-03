3fea2654f1410ec2b623bfa79d0558b1a8ee6151
pawanfalak <pawanphalak777@gmail.com>
2020-06-03 17:52:58 +0530

    New tweets appended and ordered 30

--- tweets/views.py
+++ tweets/views.py
@@ -16,6 +16,7 @@ def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
+    #print("ajax", request.is_ajax() )
     form = TweetForm(request.POST or None)
     next_url= request.POST.get("next") or None
 
@@ -23,6 +24,8 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False)
         #do other form related logic 
         obj.save()
+        if request.is_ajax():
+            return JsonResponse(obj.serialize(), status=201) #201-> created items
         if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
@@ -31,7 +34,7 @@ def tweet_create_view(request, *args, **kwargs):
 
 def tweet_list_view(request, *args, **kwargs):
     qs= Tweets.objects.all()
-    tweets_list= [{"id":x.id, "content":x.content, "likes":random.randint(0,125)} for x in qs]
+    tweets_list= [x.serialize() for x in qs]
     data={
         "response": tweets_list
     }
