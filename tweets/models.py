211d8fcc2cb0c6da7477712dd54f253afcec7c7d
pawanfalak <pawanphalak777@gmail.com>
2020-06-04 10:38:56 +0530

    form error handling

--- tweets/models.py
+++ tweets/models.py
@@ -1,9 +1,13 @@
 import random
+from django.conf import settings
 from django.db import models
+
+User = settings.AUTH_USER_MODEL
 
 # Create your models here.
 class Tweets(models.Model):
     #id=models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE) #many users can many tweet
     content=models.TextField(blank=True,null=True)
     image = models.FileField(upload_to='images/',blank=True,null=True)
 
@@ -12,7 +16,7 @@ class Tweets(models.Model):
 
     def serialize(self):
         return {
-            "id" :self.id,
+            "id": self.id,
             "content" :self.content,
             "likes" :random.randint(0,200)
         }
--- tweets/views.py
+++ tweets/views.py
@@ -13,6 +13,7 @@ ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 # Create your views here.
 def home_view(request, *args, **kwargs):
     #return HttpResponse("<h1>Hello World!!</h1>")
+    print(request.user)
     return render(request, "pages/home.html", context={}, status=200)
 
 def tweet_create_view(request, *args, **kwargs):
@@ -29,6 +30,9 @@ def tweet_create_view(request, *args, **kwargs):
         if next_url != None and is_safe_url(next_url,ALLOWED_HOSTS):
             return redirect(next_url)
         form = TweetForm()
+    if form.errors:
+        if request.is_ajax():
+            return JsonResponse(form.errors, status=400)
     return render(request, 'components/forms.html', context={"form":form})
 
 
