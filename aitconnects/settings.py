99734ce58700bd3ccb34000a10621c057e8dc6b7
pawanfalak <pawanphalak777@gmail.com>
2020-07-17 09:12:39 +0530

    Update settings.py

--- aitconnects/settings.py
+++ aitconnects/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f4bb%45hl=tm7#3vp$t3e!enunng)z47#mkh1w5g%ws$hb)%g8'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
